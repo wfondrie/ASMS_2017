f7c6257b272bd15a37f7c7e44abf1b970040435b
wfondrie <wfondrie@umaryland.edu>
2017-05-15 13:17:16 -0400

    Ready for full lib

--- R/preProcessSpec.R
+++ R/preProcessSpec.R
@@ -10,8 +10,8 @@ preProcessSpec <- function(fileList, hws = 100) {
     spec <- trim(spec, c(1000, 2400))
     spec <- transformIntensity(spec, method="sqrt")
     spec <- smoothIntensity(spec, method="SavitzkyGolay", halfWindowSize = hws)
-    spec <- removeBaseline(spec, method="SNIP", iterations=60)
-    spec <- calibrateIntensity(spec, method="TIC")
+    spec <- removeBaseline(spec, method="SNIP", iterations = 60)
+    spec <- calibrateIntensity(spec, method = "TIC")
     
     return(spec)
 }
